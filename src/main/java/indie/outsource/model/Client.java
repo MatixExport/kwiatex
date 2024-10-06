0f7734d27067602bef964b045f080d0e37f4bcad
Jakub Kubiś <jakubkubis45@gmail.com>
2024-10-06 15:25:52 +0200

    add basic db config

--- src/main/java/indie/outsource/model/Client.java
+++ src/main/java/indie/outsource/model/Client.java
@@ -1,19 +1,26 @@
 package indie.outsource.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
+@Entity
 @AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
+@Access(AccessType.FIELD)
 public class Client {
 
-    private int id;
+    @Id
+    @GeneratedValue
+    private long id;
     private String name;
     private String surname;
     private String address;
-
     public Client(String name, String surname, String address) {
         this.name = name;
         this.surname = surname;
@@ -22,5 +29,13 @@ public class Client {
 
     public String getClientInfo(){
         return "Name: " + name + " Surname: " + surname;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
--- src/test/java/indie/outsource/TestTest.java
+++ src/test/java/indie/outsource/TestTest.java
@@ -1,12 +1,38 @@
 package indie.outsource;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import indie.outsource.model.Client;
+
 
 public class TestTest {
+
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+
 
     @Test
     void testAdd() {
         assertEquals(5, 5);
+    }
+
+    @Test
+    void testDb(){
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        Client client1 = new Client("John","Doe","6th Street");
+        em.persist(client1);
+        em.getTransaction().commit();
+
+        em.getTransaction().begin();
+        Client client = em.find(Client.class, client1.getId());
+        em.getTransaction().commit();
+        assertEquals("John Doe", client.getName()+" "+client.getSurname());
+
+
+
     }
 }
