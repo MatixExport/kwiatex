94d8f38245d9ee3b98c44745e0c3e1dbecea0cca
Jakub Kubiś <jakubkubis45@gmail.com>
2024-10-06 13:50:17 +0200

    add dependency for jpa,hibernate,junit

---
+++ src/test/java/indie/outsource/TestTest.java
@@ -0,0 +1,12 @@
+package indie.outsource;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTest {
+
+    @Test
+    void testAdd() {
+        assertEquals(5, 5);
+    }
+}
