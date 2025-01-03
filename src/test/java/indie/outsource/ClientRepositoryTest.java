3c29b83b263d220ab3586bb38e936e973128209e
247662 Gieczyski Mateusz <mateusz.gielczynski@outlook.com>
2025-01-03 20:09:11 +0100

    fix ClientRepositoryTest

--- src/test/java/indie/outsource/ClientRepositoryTest.java
+++ src/test/java/indie/outsource/ClientRepositoryTest.java
@@ -25,39 +25,39 @@ public class ClientRepositoryTest {
 
     @Test
     public void insertTest() {
-        Client client = new Client("Imie", "Nazwisko", 10, "al politechniki");
+        Client client = new Client("insert", "Nazwisko", 10, "al politechniki");
 
         repository.save(client);
         client.setId(1);
         repository.save(client);
 
-        assertEquals(client.getId(), repository.findByName("Imie").getFirst().getId());
-        assertEquals(2, repository.findByName("Imie").size());
+        assertEquals(client.getId(), repository.findByName("insert").getFirst().getId());
+        assertEquals(2, repository.findByName("insert").size());
         assertEquals(client.getName(), repository.findById(10).getName());
     }
 
     @Test
     public void deleteTest() {
-        Client client = new Client("Imie", "Nazwisko", 10, "al politechniki");
+        Client client = new Client("delete", "Nazwisko", 10, "al politechniki");
 
         repository.save(client);
         repository.delete(client);
 
-        assertNull(repository.findByName("Imie"));
+        assertEquals(0, repository.findByName("delete").size());
         assertNull(repository.findById(10));
     }
 
     @Test
     public void updateTest() {
-        Client client = new Client("Imie", "Nazwisko", 10, "al politechniki");
+        Client client = new Client("update", "Nazwisko", 10, "al politechniki");
 
         repository.save(client);
 
-        client = new Client("Nowe Imie", "Nowe Nazwisko", 10, "al polite2chniki");
+        client = new Client("Nowe update", "Nowe Nazwisko", 10, "al polite2chniki");
 
         repository.update(client);
 
-        assertEquals(client.getId(), repository.findByName("Nowe Imie").getFirst().getId());
+        assertEquals(client.getId(), repository.findByName("Nowe update").getFirst().getId());
         assertEquals(client.getName(), repository.findById(10).getName());
     }
 }
